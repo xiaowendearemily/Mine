3e4129073e21e77731cf8c8dd4283bb45ad0e228
jiangxiaowen <504454767@qq.com>
2017-03-29 00:50:07 +0800

    响应式修改

--- js/index.js
+++ js/index.js
@@ -1,30 +1,20 @@
-var content1 = document.querySelector(".content1");
+var navparent = document.querySelector(".nav-parent");
 var nav = document.querySelector(".nav");
 var skill = document.querySelector(".skill");
 var height = document.body.clientHeight;
-var state = true;		
-var btn = document.querySelector("#bt");
-var _menu = document.querySelector("._menu");
-var _menu1 = document.querySelector("._menu1");
-var _li = document.querySelectorAll("._li a");
+var state = true;
 window.onscroll = function(){
-	//content1 距离上边框的距离
-	var _menuTop = _menu.offsetTop - window.scrollY;
-	var _top = content1.offsetTop - window.scrollY;	
+	console.log(123);
+	var _top = navparent.offsetTop - window.scrollY;
 	var _skill = skill.offsetTop - window.scrollY;
+	console.log(_top);
 	if(_top<=0){				
 		nav.style.position = "fixed";
 		nav.style.top = 0;				
 	}else{				
-		nav.style.position = "";	
+		nav.style.position = "";			
 	}
-
-	if(_menuTop <= 80){
-		_menu1.style.position = "fixed";
-		_menu1.style.top = "80px";
-	}else{
-		_menu1.style.position = "";
-	}
+	// 自动加载canvas百分比
 	if(_skill<height && state == true){
 		getCanvas("#canvas1",75,"#num1");				
 		getCanvas("#canvas2",80,"#num2");
@@ -34,7 +24,6 @@ window.onscroll = function(){
 		state = false;
 	}
 }
-
 $('.goto').click(function () {
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
@@ -42,6 +31,7 @@ $('.goto').click(function () {
     return false;
 });
 
+//canvas
 function getCanvas(id,num,num_id){//canvas id,画圆百分比,中间数字百分比id
 	var percent = document.querySelector(num_id);
 	var cvs = document.querySelector(id);
@@ -49,15 +39,15 @@ function getCanvas(id,num,num_id){//canvas id,画圆百分比,中间数字百分
 	var _num = num/100 * 360;
 	var per = 0;
 	var index = 0;			
-	ctx.lineWidth = 10;
+	ctx.lineWidth = 15;
 	ctx.strokeStyle = "red";
 	var sid = setInterval(function(){
 		if(_num<=index){					
 			clearInterval(sid);
 		}else{					
-			ctx.clearRect(0,0,240,240);//开始 结束坐标
+			ctx.clearRect(0,0,400,400);//开始 结束坐标
 			ctx.beginPath();
-			ctx.arc(190,80,60,Math.PI/180 * - 90,Math.PI/180 * (index - 90),false);
+			ctx.arc(160,80,60,Math.PI/180 * - 90,Math.PI/180 * (index - 90),false);
 			ctx.stroke();
 			index+=3;
 			if(per==num){
@@ -70,19 +60,6 @@ function getCanvas(id,num,num_id){//canvas id,画圆百分比,中间数字百分
 	},1000/60);
 }
 
-// $('.VivaTimeline').vivaTimeline();
-btn.onclick=function(){
-	if(_menu.style.visibility == "hidden" || _menu.style.visibility == ""){
-		_menu.style.visibility = "visible";
-		console.log(_menu.style.visibility);
-	}else{
-		_menu.style.visibility = "hidden";
-		console.log(_menu.style.visibility);
-	}	
-}
-_li.forEach(function(c,i){
-	c.onclick=function(){
-		_menu.style.visibility = "hidden";
-	}
-})
-
+$(document).ready(function(e){
+    $('.cntl').cntl();
+});
